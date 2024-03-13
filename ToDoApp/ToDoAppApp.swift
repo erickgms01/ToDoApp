c46e24b84a4d5bddc7964e351d5c422dcb162698
erickgms01 <erickderi98@gmail.com>
2024-03-13 18:20:28 -0300

    initial commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Francisco Erick Souza Gomes on 13/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
